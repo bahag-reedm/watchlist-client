bcb47cca9d2df90ef72a279aa157ffdd756ac987
Maximilian Reed <maximilian.reed@bahag.com>
2026-04-10 14:12:20 +0200

    hardcoded allow list

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    allowedHosts: ["movies-client-max-630243095989.europe-west1.run.app"],
+    allowedHosts: [
+      "movies-client-max-630243095989.europe-west1.run.app",
+      "movies-max-frontend-630243095989.europe-west1.run.app",
+    ],
     host: "0.0.0.0",
     port: "8080",
   },
